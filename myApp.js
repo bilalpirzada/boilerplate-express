7489eb6ebcbfef32c493f6db4e959b9b4958e648
Muhammad Bilal <bilal.se74@gmail.com>
2024-02-02 12:52:52 +0500

    added the first middleware to serve static assets

--- myApp.js
+++ myApp.js
@@ -1,10 +1,13 @@
 let express = require("express");
 let app = express();
 
-var absolutePath = __dirname + "/views/index.html";
+var absolutePath = __dirname + "/public";
+var indexPath = __dirname + "/views/index.html";
+
+app.use("/public", express.static(absolutePath));
 
 app.get("/", function (req, res) {
-  res.sendFile(absolutePath);
+  res.sendFile(indexPath);
 });
 
 module.exports = app;
