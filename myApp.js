d2a050b9f307e8ed6cbd66c6d9cbe9d14c818847
Muhammad Bilal <bilal.se74@gmail.com>
2024-02-02 12:32:52 +0500

    completed serve an html file

--- myApp.js
+++ myApp.js
@@ -1,39 +1,10 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
+var absolutePath = __dirname + "/views/index.html";
 
+app.get("/", function (req, res) {
+  res.sendFile(absolutePath);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
